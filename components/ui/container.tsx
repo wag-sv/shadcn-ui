41aa2c2af85adc04998991ba4088e6cd9f73aed0
Wagner Silveira Vieira <wvieira@psgtecnologia.com.br>
2023-09-03 18:05:46 -0400

    Version 0.1.0 finished.

---
+++ components/ui/container.tsx
@@ -0,0 +1,11 @@
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+export const Container: React.FC<ContainerProps> = ({ children }) => {
+  return (
+    <div className="mx-auto w-full max-w-7xl">
+      { children }
+    </div>
+  );
+};
